4b3dcf1524d67374c3e5abf676c1dc11b4c13979
Jittania <jittania@gmail.com>
2021-07-27 15:27:14 -0700

    Initial Commit

---
+++ elpy/elpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  elpyApp.swift
+//  elpy
+//
+//  Created by Jittania Smith on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct elpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
